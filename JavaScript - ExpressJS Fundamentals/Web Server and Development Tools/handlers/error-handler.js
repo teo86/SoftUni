3101e0fddb19af2d57791a5f4cf43e6c198d9de4
teo86 <teo_1986@mail.bg>
2019-01-25 23:59:57 +0200

    add -> Web Server and Development Tools

---
+++ JavaScript - ExpressJS Fundamentals/Web Server and Development Tools/handlers/error-handler.js
@@ -0,0 +1,18 @@
+module.exports = function notFound(res, errHandleCb){
+    res.writeHead(404, {
+      'Content-Type': 'text/plain'
+    })
+  
+    res.write('404 not found')
+    res.end();
+  
+    let errorText = res.pathname + ' 404 snot found';
+  
+    if(errHandleCb){
+      errHandleCb(errorText);
+    }else {
+      console.log(errorText);
+    }
+  
+    return;
+  }
---
+++ JavaScript - ExpressJS Fundamentals/Web Server and Development Tools/handlers/index.js
@@ -0,0 +1,5 @@
+const homeHandler = require('./home');
+const staticFilesHandler = require('./static-files');
+const allMoviesHandler = require('./all-movies');
+const addMovieHandler = require('./add-movie');
+const singleMoviePageHandler = require('./single-movie-page')
